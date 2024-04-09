95107ceea0d7284ff9e2f4b2d01e20474e5d571d
Vikki <vikas@hood.live>
2024-04-09 15:26:15 +0530

    Basic java spring boot application

---
+++ src/main/java/com/designofox/hyremap/HyremapApplication.java
@@ -0,0 +1,13 @@
+package com.designofox.hyremap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyremapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyremapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designofox/hyremap/HyremapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designofox.hyremap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyremapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
